a127c283d8689490b3f1fe6492c196dc9e66167f
sw1313 <hfzy2014@gmail.com>
2025-08-13 08:58:46 +0800

    Create AssemblyInfo.cs

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("QuickSearchNotesHelper")]
+[assembly: AssemblyDescription("QuickSearch source for searching Playnite games by name/notes/description")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("QuickSearchNotesHelper")]
+[assembly: AssemblyCopyright("Copyright © 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("48CFBCFC-545D-4737-AAAE-B495A5E2BEA6")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
